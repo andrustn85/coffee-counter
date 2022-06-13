131986ceb6a678b54f9b7e026f0123a7e9f7050f
andrustn85 <rustandres@gmail.com>
2022-06-13 16:47:15 +0200

    Updates

---
+++ precache-manifest.fbb1c3be1c81ecde5df4c990df713124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9780a804a1d681038f1f",
+    "url": "/andrustn85/coffee-counter.git/static/css/main.b694fe02.chunk.css"
+  },
+  {
+    "revision": "9780a804a1d681038f1f",
+    "url": "/andrustn85/coffee-counter.git/static/js/main.9780a804.chunk.js"
+  },
+  {
+    "revision": "1a6b9e4147af585497e3",
+    "url": "/andrustn85/coffee-counter.git/static/js/1.1a6b9e41.chunk.js"
+  },
+  {
+    "revision": "ff20d510bd7a8d58af7b",
+    "url": "/andrustn85/coffee-counter.git/static/js/runtime~main.ff20d510.js"
+  },
+  {
+    "revision": "187768612c4dead5ab1b24ca71c31ae1",
+    "url": "/andrustn85/coffee-counter.git/static/media/starbuckslogo.18776861.png"
+  },
+  {
+    "revision": "122ec9d8a5b6f73f12855f3c5bb20194",
+    "url": "/andrustn85/coffee-counter.git/index.html"
+  }
+];
